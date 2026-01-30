0074030143f1e57261f0e9ba9343d55283ec1a96
Emmanuel Adekoya <iamdamified@gmail.com>
2026-01-30 22:18:56 +0100

    array operations

--- Data_science/array_manipulations.py
+++ Data_science/array_manipulations.py
@@ -1,3 +1,35 @@
 # Chnage Shapes of Arrays, and Adding Dimensions
 import numpy as np
 
+# reshape
+arr = np.array([1,2,3,4,5,6])
+reshaped = arr.reshape((2,3))
+reshaped1 = arr.reshape((3,2))
+print(reshaped)
+print(reshaped1)
+
+# expansion into new axis
+arr = np.array([1,2,3])
+expanded = arr[:, np.newaxis]
+print(expanded)
+
+
+# Basic Arrays Operations
+
+# 1. Element-wise
+a = np.array([1,2,3])
+b = np.array([4,5,6])
+print(a+b)
+print(a*b)
+print(a/b)
+
+# 2. Mathematical
+arr = np.array([4,16,25])
+print(np.sqrt(arr))
+print(np.sum(arr))
+print(np.mean(arr))
+print(np.max(arr))
+
+
+
+# Array Indexing, Slicing, and Reshaping
